c7eeb056adaef1f244cc4100e77f7aa9953400c5
Tabea Frenzel <tabea.frenzel@parlamind.com>
2021-10-22 11:33:08 +0200

    [master] First basic layout.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,59 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container grid-list-md>
+        <v-row>
+          <Card v-for="path in paths" :key="path.pathTitle" v-bind:path="path" />
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Card from './components/Card.vue';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Card
   },
 
   data: () => ({
-    //
-  }),
+    paths: [
+      {
+        pathTitle: 'Test 1',
+        path: 'M 123234',
+        width: 6
+      },
+      {
+        pathTitle: 'Test 2',
+        path: '23432432',
+        width: 6
+      },
+      {
+        pathTitle: 'Test 3',
+        path: '34543543',
+        width: 4
+      },
+      {
+        pathTitle: 'Test 4',
+        path: '757456',
+        width: 4
+      },
+      {
+        pathTitle: 'Test 5',
+        path: '23424i',
+        width: 4
+      },
+      {
+        pathTitle: 'Test 6',
+        path: 'C 234252',
+        width: 12
+      }
+    ]
+  })
 };
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-col :cols="path.width">
+    <v-card tile>
+        <v-card-title>{{path.pathTitle}}</v-card-title>
+        <v-card-text>
+            <svg></svg>
+            <small>{{path.path}}</small>
+        </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['path']
+};
+</script>
+
+<style scoped>
+svg {
+    width: 100%;
+    background-color: blue;
+}
+</style>
